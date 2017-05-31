8ae96e6a1e51c62822ccb8ed9e3b953360953288
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-31 13:05:51 +0300

    fix entry script

--- docker/s3-config-reader/entry.sh
+++ docker/s3-config-reader/entry.sh
@@ -5,18 +5,19 @@
 if [ -z "$PATH" ]; then
     echo "Environment variable PATH is not set!!!"
     exit 1
-
+fi
 if [ -z "$BUCKET" ]; then
     echo "Environment variable BUCKET is not set!!!"
     exit 1
-
+fi
 if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
     echo "Environment variable AWS_SECRET is not set!!!"
     exit 1
-
+fi
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
     echo "Environment variable AWS_SECRET is not set!!!"
     exit 1
+fi
 
 # Create nginx supervisord log folder
 mkdir -p /var/log/nginx
